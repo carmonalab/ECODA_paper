5364c690be21e8329ac5a408930ca53fbcf947bf
halterc <chalterbio@gmail.com>
2026-07-08 09:45:07 +0200

    Updated src folder structure

--- src/R/load_all_functions.R
+++ src/R/load_all_functions.R
@@ -4,14 +4,14 @@
 # ============================================================
 
 # Load all modules in dependency order
-source("R/imports.R")           # Package imports
-source("R/constants.R")         # Constants & configurations
-source("R/helpers.R")           # Utility helpers (exec_time, etc.)
-source("R/math_utils.R")        # Math/statistics utilities (clr, cv, impute_zeros)
-source("R/scoring_metrics.R")   # Separation scoring & clustering metrics
-source("R/pseudobulk.R")        # Pseudobulk & DESeq2 functions
-source("R/hvcs.R")              # Highly Variable Cell Type functions
-source("R/seurat_utils.R")      # Seurat object utilities
-source("R/plotting.R")          # PCA, MDS plotting functions
-source("R/benchmark_methods.R") # Individual method processing functions
-source("R/pipeline.R")          # Pipeline orchestration (run_benchmark, run_transformation_analysis, etc.)
+source("src/R/imports.R")           # Package imports
+source("src/R/constants.R")         # Constants & configurations
+source("src/R/helpers.R")           # Utility helpers (exec_time, etc.)
+source("src/R/math_utils.R")        # Math/statistics utilities (clr, cv, impute_zeros)
+source("src/R/scoring_metrics.R")   # Separation scoring & clustering metrics
+source("src/R/pseudobulk.R")        # Pseudobulk & DESeq2 functions
+source("src/R/hvcs.R")              # Highly Variable Cell Type functions
+source("src/R/seurat_utils.R")      # Seurat object utilities
+source("src/R/plotting.R")          # PCA, MDS plotting functions
+source("src/R/benchmark_methods.R") # Individual method processing functions
+source("src/R/pipeline.R")          # Pipeline orchestration (run_benchmark, run_transformation_analysis, etc.)
